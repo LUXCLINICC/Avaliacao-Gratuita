efef50b15bba715c47e5fe3d8c0c31326995d1c6
LUXCLINICC <geral@luxclinic.pt>
2026-07-14 18:44:57 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -207,5 +207,11 @@ if (formulario) {
         }
 
     });
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav");
 
+if (menuToggle && nav) {
+    menuToggle.addEventListener("click", () => {
+        nav.classList.toggle("active");
+    });
 }
